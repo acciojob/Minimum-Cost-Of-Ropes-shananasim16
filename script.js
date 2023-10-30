8000119b59b59cfe3fb79c75d07e4c9a97e28e6d
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-30 12:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 function calculateMinCost() {
-  const ropeLengthsInput = document.getElementById('rope-lengths');
-  const resultDiv = document.getElementById('result');
-  const ropeLengths = ropeLengthsInput.value.split(',').map(Number);
-   resultDiv.textContent = findMinCostRopes(ropeLengths);;
+  const roper= document.getElementById("rope-lengths")
+	let lenarr= (roper.value).split(',')
+	let sum=0;
+	let fs= lenarr.map(x => Number(x))
+	let gh=0
+	while(fs.length!=1){
+		fs.sort(function(a,b){return a-b})
+		sum=fs[0]+fs[1]
+		fs[1]=sum
+		fs.shift()
+		gh=gh+sum
+	}
+	const reser= document.getElementById("result")
+	reser.innerText= gh
 }
-
-function findMinCostRopes(arr) {
-    let minCost = 0;
-    let minHeap = [];
-    for (let i = 0; i < arr.length; i++) {
-      minHeap.push(arr[i]);
-      minHeap.sort((a, b) => a - b);
-    }
-    while (minHeap.length > 1) {
-      const first = minHeap.shift();
-      const second = minHeap.shift();
-      const cost = first + second;
-      minCost += cost;
-      minHeap.push(cost);
-      minHeap.sort((a, b) => a - b);
-    }
-   return minCost;
-  }
